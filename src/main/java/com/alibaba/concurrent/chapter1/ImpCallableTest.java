15ff0b0d6216b749361572811f9a77a3960ffada
shenmeng <852051958@qq.com>
2019-11-26 21:36:21 +0800

    并发编程基础

--- src/main/java/com/alibaba/concurrent/chapter1/ImpCallableTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/ImpCallableTest.java
@@ -10,18 +10,19 @@ import java.util.concurrent.FutureTask;
  **/
 
 public class ImpCallableTest {
-
     public static class CallerTask implements Callable<String>{
         @Override
         public String call() throws Exception {
             return "I am callable";
         }
     }
-
     public static void main(String[] args) {
+        //创建异步任务
         FutureTask<String> futureTask =new FutureTask<>(new CallerTask());
+        //启动线程
         new Thread(futureTask).start();
         try {
+            //等待任务执行完毕，并返回结果。
             String s = futureTask.get();
             System.out.println("result==="+s);
         } catch (InterruptedException e) {
