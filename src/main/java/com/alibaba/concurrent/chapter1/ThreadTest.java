d830f73a96921bb76e21c763bb77add0d31886f5
shenmeng <852051958@qq.com>
2019-11-26 20:33:36 +0800

    并发编程基础

--- src/main/java/com/alibaba/concurrent/chapter1/ThreadTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/ThreadTest.java
@@ -6,4 +6,17 @@ package com.alibaba.concurrent.chapter1;
  **/
 
 public class ThreadTest {
+
+    public static class MyThread extends Thread{
+
+        @Override
+        public void run() {
+            System.out.println("I am a child thread!");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread=new MyThread();
+        myThread.start();
+    }
 }
