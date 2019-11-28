079707106f97d8f9ebdb181bd0751a6f822f3053
shenmeng <852051958@qq.com>
2019-11-28 10:47:44 +0800

    Join

--- src/main/java/com/alibaba/concurrent/chapter1/SleepInterruptTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/SleepInterruptTest.java
@@ -6,4 +6,30 @@ package com.alibaba.concurrent.chapter1;
  **/
 
 public class SleepInterruptTest {
+
+    public static void main(String[] args) {
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                System.out.println("threadA begin to sleep");
+                    Thread.sleep(10000);
+                    System.out.println("threadA end sleep");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadA.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //主线程中断子线程
+        threadA.interrupt();
+    }
 }
--- src/main/java/com/alibaba/concurrent/chapter1/SleepTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/SleepTest.java
@@ -1,9 +1,54 @@
 package com.alibaba.concurrent.chapter1;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Author shenmeng
  * @Date 2019/11/28
  **/
 
-public class SleepTest {
+public class SleepTest  {
+
+    private static final Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //获取独占锁
+                lock.lock();
+                try {
+                    System.out.println("threadA is in sleep!");
+                    Thread.sleep(10000);
+                    System.out.println("threadA is in awaked");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //获取独占锁
+                lock.lock();
+                try {
+                    System.out.println("threadB is in sleep!");
+                    Thread.sleep(10000);
+                    System.out.println("threadB is in awaked");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+    }
 }
