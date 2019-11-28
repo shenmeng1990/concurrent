71acc3dd158e775b0664de6d0584e70a86a52936
shenmeng <852051958@qq.com>
2019-11-28 10:21:05 +0800

    Join

--- src/main/java/com/alibaba/concurrent/chapter1/JoinInterruptTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/JoinInterruptTest.java
@@ -6,4 +6,43 @@ package com.alibaba.concurrent.chapter1;
  **/
 
 public class JoinInterruptTest {
+
+    public static void main(String[] args) {
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("threadA begin run!");
+                for(;;){
+
+                }
+            }
+        });
+
+        final Thread mainThread = Thread.currentThread();
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                mainThread.interrupt();
+            }
+        });
+
+        threadA.start();
+
+        threadB.start();
+
+        try {
+            threadA.join();
+        } catch (InterruptedException e) {
+            System.out.println("main thread "+e);
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/alibaba/concurrent/chapter1/JoinTest.java
+++ src/main/java/com/alibaba/concurrent/chapter1/JoinTest.java
@@ -6,4 +6,43 @@ package com.alibaba.concurrent.chapter1;
  **/
 
 public class JoinTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("ThreadA is over!");
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("ThreadB is over!");
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+
+        System.out.println("wait all child thread over!");
+
+        //等待子线程执行完毕，返回。
+        threadA.join();
+        threadB.join();
+
+        System.out.println("all thread is over!");
+    }
+
 }
